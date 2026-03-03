dc76ccea1025ceec68283e0560d8f529f143351b
hanabi2356home <hanabira2356@gmail.com>
2026-03-04 02:51:36 +0900

    ThrowIfFailed 완성

---
+++ Diablo/AppUtility.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include<windows.h>
+#include<cassert>
+#include<wrl.h>
+#include<DirectXMath.h>
+#include<d3d12.h>
+#include<string>
+#include<dxgi.h>
+#include<stringapiset.h>
+#include<comdef.h>
+
+inline std::wstring AnsiToWString(const std::string& str)
+{
+	WCHAR buffer[512];
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
+	return std::wstring(buffer);
+}
+class AppUtility
+{
+};
+class DxException
+{
+public:
+	DxException() = default;
+	DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& fileName, int lineNumber);
+
+	std::wstring ToString()const;
+
+	HRESULT ErrorCode = S_OK;
+	std::wstring FunctionName;
+	std::wstring FileName;
+	int LineNumber = -1;
+};
+
+#ifndef ThrowIfFailed
+#define ThrowIfFailed(x) \
+{\
+	HRESULT hr__ = (x); \
+	std::wstring wfn = AnsiToWString(__FILE__); \
+	if(FAILED(hr__)) { throw DxException(hr__, L#x, wfn, __LINE__); } \
+}
+#endif
